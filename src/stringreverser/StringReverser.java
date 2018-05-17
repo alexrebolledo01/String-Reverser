2cb2cdedd6caa0a28d902d597746b5488665edd9
Alex <alexrebolledo01@gmail.com>
2018-05-17 06:56:58 -0400

    Added class description.

---
+++ src/stringreverser/StringReverser.java
@@ -0,0 +1,29 @@
+/* 
+ * PROJECT:    String reverser
+ * DATE:       May 15, 2018
+ * AUTHOR:     Alex Rebolledo
+ * NOTES:      Interview preparation
+ */
+package stringreverser;
+
+/**
+ * Reverses a string object using a StringBuilder.
+ * @author Alex Rebolledo
+ */
+public class StringReverser {
+    
+    public StringBuilder reverseString(String word) {
+        
+        StringBuilder reverseBuilder = new StringBuilder();
+        
+        char[] wordCharArray = word.toCharArray();
+        
+        for (int counter = (wordCharArray.length); counter > 0; counter--) {
+            System.out.println(wordCharArray[counter - 1]);
+            
+            reverseBuilder.append(wordCharArray[counter - 1]);
+        }
+        
+        return reverseBuilder;
+    }
+}
